4a7e28cef16664c2d15c1c501c0b5bb1e8e56e98
Connor Perez <connorperez@gmail.com>
2019-07-25 08:51:04 -0500

    cleaned up code

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -3,26 +3,6 @@ import Jumbotron from './components/Jumbotron';
 import MainGameContainer from "./components/MainGameContainer";
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 const App = () => (
   <div className="container-fluid mainContainer">
