07dc6b2268138f1d462e69c46779f6e24cdf9ec0
Connor Perez <connorperez@gmail.com>
2019-07-25 14:30:09 -0500

    Updates

---
+++ precache-manifest.19e2f96f95351bab74a61cc04c1438f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c6b119e36b96364cc6aada36bc2ab9",
+    "url": "/South-Park-Clicky-Memory-Game/index.html"
+  },
+  {
+    "revision": "f7fa3e060af02ee2efff",
+    "url": "/South-Park-Clicky-Memory-Game/static/css/main.1ed5b683.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/South-Park-Clicky-Memory-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f7fa3e060af02ee2efff",
+    "url": "/South-Park-Clicky-Memory-Game/static/js/main.313f7108.chunk.js"
+  },
+  {
+    "revision": "96a80e04b871d434b582",
+    "url": "/South-Park-Clicky-Memory-Game/static/js/runtime~main.f97d5ade.js"
+  }
+]);
